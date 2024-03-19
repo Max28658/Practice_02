3dcc2e431f9af502cb3ce44308b0019689815f8e
s28658 <s28658@pjwstk.edu.pl>
2024-03-19 18:04:43 +0100

    Modification 5

--- Practice_02/Practice_02/Program.cs
+++ Practice_02/Practice_02/Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
 Console.WriteLine("Modification 4");
+Console.WriteLine("Modification 5");
 
 
 
