ec11835549df37469e8a87ed13a41af5fdccc859
s28658 <s28658@pjwstk.edu.pl>
2024-03-19 17:28:01 +0100

    Modification 3

--- Practice_02/Practice_02/Program.cs
+++ Practice_02/Practice_02/Program.cs
@@ -5,6 +5,7 @@ using Practice_02;
 Console.WriteLine("initial project commit push!");
 Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
+Console.WriteLine("Modification 3");
 
 int[] arr = [1, 3, 45, 2, 45, 2, 5, 45, 2, 345, 2, 345];
 Console.WriteLine("avg of array" + Task03.countAvg(arr));
