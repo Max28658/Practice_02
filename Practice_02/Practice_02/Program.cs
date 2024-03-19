e7afe9d22a8b8a83523ac3b3be74332d40ec8dcd
s28658 <s28658@pjwstk.edu.pl>
2024-03-19 17:52:50 +0100

    Feature-max Modification 1

--- Practice_02/Practice_02/Program.cs
+++ Practice_02/Practice_02/Program.cs
@@ -15,5 +15,10 @@ Console.WriteLine("Feature-average Modification 1");
 Console.WriteLine("Feature-average Modification 2");
 Console.WriteLine("Feature-average Modification 3");
 
+Console.WriteLine("Feature-max Modification 1");
+
+
+
 int[] arr = [1, 3, 45, 2, 45, 2, 5, 45, 2, 345, 2, 345];
 Console.WriteLine("avg of array" + Task03.countAvg(arr));
+Console.WriteLine("max of array" + Task04.findMax(arr));
--- Practice_02/Practice_02/Task04.cs
+++ Practice_02/Practice_02/Task04.cs
@@ -2,5 +2,27 @@ namespace Practice_02;
 
 public class Task04
 {
-    
+    public static int findMax(int[] arr)
+    {
+        if (arr == null)
+        {
+            return 0;
+        }
+        else if (arr.Length == 1)
+        {
+            return arr[0];
+        }
+
+        int max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+        }
+
+        return max;
+
+    }
 }
