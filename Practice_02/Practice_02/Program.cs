924f70944da5b15095b46a092687c9c61f8ac5d6
s28658 <s28658@pjwstk.edu.pl>
2024-03-19 17:37:39 +0100

    Feature-average Modification 2

--- Practice_02/Practice_02/Program.cs
+++ Practice_02/Practice_02/Program.cs
@@ -10,6 +10,7 @@ Console.WriteLine("Modification 3");
 
 
 Console.WriteLine("Feature-average Modification 1");
+Console.WriteLine("Feature-average Modification 2");
 
 int[] arr = [1, 3, 45, 2, 45, 2, 5, 45, 2, 345, 2, 345];
 Console.WriteLine("avg of array" + Task02.countAvg(arr));
